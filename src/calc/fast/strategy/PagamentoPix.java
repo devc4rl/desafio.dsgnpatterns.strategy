36dea045357604909182f9fbbb147303af26d14b
devc4rl <cg73406@gmail.com>
2025-08-27 16:00:45 -0300

    feat: Adicionando o projeto de frete com Padrão Strategy

---
+++ src/calc/fast/strategy/PagamentoPix.java
@@ -0,0 +1,10 @@
+package calc.fast.strategy;
+
+public class PagamentoPix implements PagamentoStrategy {
+
+    @Override
+    public void pagar(double valor) {
+        System.out.println("Pagamento de R$ " + valor + " realizado com Pix.");
+
+    }
+}
